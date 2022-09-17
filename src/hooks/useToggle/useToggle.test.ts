fa9d47c696931e1aa11c51fa565040515a704711
Eder Sampaio <edersampaio@outlook.com.br>
2022-09-17 12:18:07 -0300

    feat: add hook

--- src/hooks/useToggle/useToggle.test.ts
+++ src/hooks/useToggle/useToggle.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks'
+import { renderHook } from '@testing-library/react'
 import useToggle from './useToggle'
 
 describe('useToggle | hook | integration test', () => {
